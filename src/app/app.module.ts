0196aa2326aacee06129466011c9b32a05dc11e0
DOREEM <Magico>
2020-11-04 16:54:53 +0100

    node add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    HomeComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
